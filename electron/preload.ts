9a0671961f3ee4377e42f0e9624252d8c77166a7
RohanShety1 <142515828+RohanShety1@users.noreply.github.com>
2026-04-12 16:57:20 -0400

    fix: use ipcRenderer.once for setup-error to prevent listener accumulation

--- electron/preload.ts
+++ electron/preload.ts
@@ -8,6 +8,6 @@ contextBridge.exposeInMainWorld('electron', {
   setupComplete: (data: object) => ipcRenderer.send('setup-complete', data),
   getConfig: () => ipcRenderer.invoke('get-config'),
   onSetupError: (cb: (error: string) => void) => {
-    ipcRenderer.on('setup-error', (_e, error: string) => cb(error))
+    ipcRenderer.once('setup-error', (_e, error: string) => cb(error))
   },
 })
